279e7da835447b951aa917239df378841b328fe0
GLProgrammer <jirka2002@gmail.com>
2017-01-02 08:46:35 +0100

    Added Vector2 class
    Added Vector2 class

---
+++ Game engine/Game engine/Vector2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_engine
+{
+    class Vector2
+    {
+        private int x, y;
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int GetX()
+        {
+            return x;
+        }
+        public int GetY()
+        {
+            return y;
+        }
+        public Vector2 GetZero()
+        {
+            return new Vector2(0, 0);
+        }
+        public void SetX(int x)
+        {
+            this.x =x;
+        }
+        public void SetY(int y)
+        {
+            this.y = y;
+        }
+        public void SetVector(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
