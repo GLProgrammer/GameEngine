651bb5050150fd2aec7d8b3a8808fa9e1a322980
GLProgrammer <jirka2002@gmail.com>
2017-01-02 13:15:29 +0100

    ADDED MOUSE

---
+++ Game engine/Game engine/Mouse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_engine
+{
+    class Mouse
+    {
+        public Vector2 GetMousePosition()
+        {
+            return new Vector2((float)Cursor.Position.X, (float)Cursor.Position.Y);           
+        }
+        
+    }
+}
