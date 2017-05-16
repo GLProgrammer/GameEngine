408c901abc9a0d08b1b336efe9d5dd03c171d3b6
GLProgrammer <jirka2002@gmail.com>
2017-05-16 08:10:46 +0200

    added basic game loop
    added basic game loop

---
+++ Game engine/Game engine/GameCore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Game_engine
+{
+    class GameCore
+    {
+        private int RecomenedFPS;
+        private float RecomendedTimeOnFrame;
+        private float TimeBefore;
+        private float TimeAfter;
+        private float RealFPS;
+        public void FluidGameLoop(int recommendedFPS)
+        {
+            Timer timer = new Timer();
+            RecomendedTimeOnFrame = 1 / RecomenedFPS;  //Čas pro 1 frame
+            while (true)
+            {
+                TimeBefore = 0;
+                timer.Start();
+                GetInput();
+                Update();
+                Render();
+                timer.Stop();
+                TimeAfter = (float)(timer.Interval);
+                RealFPS = 1.0f / TimeAfter;
+            }
+            
+        }
+
+        private void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
